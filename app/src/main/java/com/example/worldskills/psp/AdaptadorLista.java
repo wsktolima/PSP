8740ff33b3e3dcbb6d57e05d7c3a09ca8096c663
Sebastian <bgjuan8@misena.edu.co>
2018-09-01 08:46:37 -0500

    Inicio

--- app/src/main/java/com/example/worldskills/psp/AdaptadorLista.java
+++ app/src/main/java/com/example/worldskills/psp/AdaptadorLista.java
@@ -1,4 +1,33 @@
 package com.example.worldskills.psp;
 
-public class AdaptadorLista {
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AdaptadorLista extends RecyclerView.Adapter<AdaptadorLista.ListaViewHolder> {
+
+    @Override
+    public ListaViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View layout = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_lista, null);
+        RecyclerView.LayoutParams p = new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layout.setLayoutParams(p);
+        return new ListaViewHolder(layout);
+    }
+
+    @Override
+    public void onBindViewHolder(ListaViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ListaViewHolder extends RecyclerView.ViewHolder {
+        public ListaViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
