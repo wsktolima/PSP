2b2fef9501e3886df8e76a7c02c5e49ab1d80ea0
Juancho330 <42784608+Juancho330@users.noreply.github.com>
2018-09-01 11:36:01 -0500

    push

---
+++ app/src/main/java/com/example/worldskills/psp/Activities/Formulariodefaultlog.java
@@ -0,0 +1,15 @@
+package com.example.worldskills.psp.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.worldskills.psp.R;
+
+public class Formulariodefaultlog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulariodefaultlog);
+    }
+}
